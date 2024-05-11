33d31c9f344e6586cfb2ce06e202f4e4932427e4
Antonín Kazda <antoninkazda@seznam.cz>
2024-05-11 14:47:29 +0200

    feat: add basic Program functionality

--- DeepLTranslateTool/Program.cs
+++ DeepLTranslateTool/Program.cs
@@ -1,9 +1,37 @@
-﻿namespace DeepLTranslateTool;
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace DeepLTranslateTool;
 
 class Program
 {
+    public class Options
+    {
+        [Option('a', "adapter", Default = "plaintext", HelpText = "Adapter to use for input and output files.")]
+        public string? InputFile { get; set; }
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello world!");
+        var parser = new Parser(with => with.HelpWriter = null);
+        var parserResult = parser.ParseArguments<Options>(args);
+        parserResult.WithParsed(options => Run(options)).WithNotParsed(errs => DisplayHelp(parserResult));
+    }
+
+    static void DisplayHelp<T>(ParserResult<T> result) =>
+        Console.WriteLine(
+            HelpText.AutoBuild(
+                result,
+                helpText =>
+                {
+                    helpText.AdditionalNewLineAfterOption = false;
+                    return helpText;
+                }
+            )
+        );
+
+    private static void Run(Options options)
+    {
+        //do stuff
     }
 }
